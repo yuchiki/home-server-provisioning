b7a8e6ce67822ada4b3f74599250722716dee1ab
yuchiki <14884013+yuchiki@users.noreply.github.com>
2023-04-29 16:48:54 +0900

    VPN Stack を作成 (#4)

--- CDK/lib/vpcStack.ts
+++ CDK/lib/vpcStack.ts
@@ -1,11 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
-import { IpAddresses, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { CfnEIP, CfnKeyPair, IpAddresses, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 import { StackConfig } from './stackConfig';
 import { vpcCidr } from './vpcCidr';
 
 export class VPCStack extends cdk.Stack {
     public readonly vpc: Vpc
+    public readonly vpnElasticIP: CfnEIP;
 
     constructor(scope: Construct, idPrefix: string, stackConfig: StackConfig, readonly props?: cdk.StackProps) {
         super(scope, `${idPrefix}-${stackConfig.targetEnvironment}`, props);
@@ -19,5 +20,24 @@ export class VPCStack extends cdk.Stack {
             }
         );
 
+        this.vpnElasticIP = new CfnEIP(this, "Elastic IP for VPN",
+        {
+            tags: [
+                {
+                    key: "Name",
+                    value: "VPNEIP",
+                },
+                {
+                    key: "Environment",
+                    value: stackConfig.targetEnvironment,
+                },
+                {
+                    key: "Project",
+                    value: "VPN",
+                },
+            ]
+        });
+
+        new cdk.CfnOutput(this, 'vpnElasticIP', { value: this.vpnElasticIP.ref});
     }
 }
