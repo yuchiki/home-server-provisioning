81ee47f1d27a2e089bb40bd3f798a451e0c6fe17
yuchiki <yuki082821993@gmail.com>
2023-06-03 23:17:02 +0900

    prettierをかけた

--- CDK/lib/vpcCidr.ts
+++ CDK/lib/vpcCidr.ts
@@ -1,8 +1,7 @@
 import { TargetEnvironment } from "./targetEnvironment";
 
-export const vpcCidr : {[P in TargetEnvironment]: string} = {
-    "production" : "10.0.0.0/16",
-    "staging": "10.1.0.0/16",
-    "development": "10.2.0.0/16",
+export const vpcCidr: { [P in TargetEnvironment]: string } = {
+  production: "10.0.0.0/16",
+  staging: "10.1.0.0/16",
+  development: "10.2.0.0/16",
 };
-
--- CDK/lib/vpcStack.ts
+++ CDK/lib/vpcStack.ts
@@ -1,43 +1,43 @@
-import * as cdk from 'aws-cdk-lib';
-import { CfnEIP, CfnKeyPair, IpAddresses, Vpc } from 'aws-cdk-lib/aws-ec2';
-import { Construct } from 'constructs';
-import { StackConfig } from './stackConfig';
-import { vpcCidr } from './vpcCidr';
+import * as cdk from "aws-cdk-lib";
+import { CfnEIP, CfnKeyPair, IpAddresses, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+import { StackConfig } from "./stackConfig";
+import { vpcCidr } from "./vpcCidr";
 
 export class VPCStack extends cdk.Stack {
-    public readonly vpc: Vpc
-    public readonly vpnElasticIP: CfnEIP;
+  public readonly vpc: Vpc;
+  public readonly vpnElasticIP: CfnEIP;
 
-    constructor(scope: Construct, idPrefix: string, stackConfig: StackConfig, readonly props?: cdk.StackProps) {
-        super(scope, `${idPrefix}-${stackConfig.targetEnvironment}`, props);
+  constructor(
+    scope: Construct,
+    idPrefix: string,
+    stackConfig: StackConfig,
+    readonly props?: cdk.StackProps
+  ) {
+    super(scope, `${idPrefix}-${stackConfig.targetEnvironment}`, props);
 
-        this.vpc = new Vpc(
-            this,
-            `vpc`,
-            {
-                ipAddresses: IpAddresses.cidr(vpcCidr[stackConfig.targetEnvironment]),
-                maxAzs: 2,
-            }
-        );
+    this.vpc = new Vpc(this, `vpc`, {
+      ipAddresses: IpAddresses.cidr(vpcCidr[stackConfig.targetEnvironment]),
+      maxAzs: 2,
+    });
 
-        this.vpnElasticIP = new CfnEIP(this, "Elastic IP for VPN",
+    this.vpnElasticIP = new CfnEIP(this, "Elastic IP for VPN", {
+      tags: [
         {
-            tags: [
-                {
-                    key: "Name",
-                    value: "VPNEIP",
-                },
-                {
-                    key: "Environment",
-                    value: stackConfig.targetEnvironment,
-                },
-                {
-                    key: "Project",
-                    value: "VPN",
-                },
-            ]
-        });
+          key: "Name",
+          value: "VPNEIP",
+        },
+        {
+          key: "Environment",
+          value: stackConfig.targetEnvironment,
+        },
+        {
+          key: "Project",
+          value: "VPN",
+        },
+      ],
+    });
 
-        new cdk.CfnOutput(this, 'vpnElasticIP', { value: this.vpnElasticIP.ref});
-    }
+    new cdk.CfnOutput(this, "vpnElasticIP", { value: this.vpnElasticIP.ref });
+  }
 }
